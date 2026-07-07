1b4a0feb27b5b3cfaa4d615273d65d3db58976d8
Octavio <3amsro@gmail.com>
2026-07-07 16:20:22 -0700

    question number 6 program

--- O Bit Manipulation/O.4 Converting integers between binary and decimal representation/q_6.cpp
+++ O Bit Manipulation/O.4 Converting integers between binary and decimal representation/q_6.cpp
@@ -2,7 +2,20 @@
 
 int main()
 {
-	int number {};
+	int input_number {};
 	std::cout << "Enter a number between 0 and 255: ";
-	std::cin >> number;
+	std::cin >> input_number;
+	int base_two_number {128};
+	std::string binary {};
+	while(base_two_number >= 1){
+	    if (input_number >= base_two_number) {
+	    	binary += "1";
+	    	input_number -= base_two_number;
+	    }
+	    else{
+	    	binary += "0";
+	    }
+		base_two_number /= 2;
+	}
+	std::cout << binary << "\n";
 }
