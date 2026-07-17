fd7b397c359e1af786d91e91d859773a014e52d8
Octavio <3amsro@gmail.com>
2026-07-17 10:48:02 -0700

    Global constants

---
+++ 7 Scope, Duration, and Linkage/7.10 Sharing global constants across multiple files/external_constants.h
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants
+{
+	// We  use extern to ensure these have external linkage
+	extern const double pi;
+	extern const double avogadro;
+	extern const double myGravity;
+}
+
+#endif
