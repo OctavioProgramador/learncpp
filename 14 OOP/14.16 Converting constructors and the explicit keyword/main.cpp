ff937fd7dde462173c642a796fa0a1faba5e0841
Octavio Salasar <octavio.salasar18@tectijuana.edu.mx>
2026-08-24 14:12:22 -0700

    The explicit keyword

---
+++ 14 OOP/14.16 Converting constructors and the explicit keyword/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void printDouble(double d) // has a double parameter
+{
+	std::cout << d;
+}
+
+int main()
+{
+	printDouble(5);	// we're supplying an int argument
+
+	return 0;
+}
+
